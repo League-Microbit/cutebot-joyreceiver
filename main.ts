bf70d878b5bb0c4b2288e1fefb88d9a2bc30a9b4
The LEAGUE of Amazing Programmers <it@jointheleague.org>
2024-08-10 19:26:58 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,9 +10,19 @@ radio.onReceivedValue(function (name, value) {
     } else {
     	
     }
+    if (x > 507 && x < 525) {
+        x = 512
+    }
+    if (y > 507 && y < 525) {
+        y = 512
+    }
 })
 let py = 0
 let px = 0
+let rw_speed = 0
+let lw_speed = 0
+let turn_speed = 0
+let fwd_speed = 0
 let y = 0
 let b = 0
 let x = 0
@@ -46,8 +56,19 @@ basic.forever(function () {
         basic.showArrow(ArrowNames.South)
     } else {
         basic.clearScreen()
+        fwd_speed = Math.map(y - 0, 0, 1023, 0, 200) - 100
+        turn_speed = Math.map(x - 0, 0, 1023, 100, 0) - 50
+        lw_speed = fwd_speed + turn_speed
+        rw_speed = fwd_speed - turn_speed
+        cuteBot.motors(lw_speed, rw_speed)
         px = Math.map(x - 100, 1023, 0, -2, 2) + 2
         py = Math.map(y - 100, 1023, 0, -2, 2) + 2
         led.plot(px, py)
+        serial.writeValue("x", x)
+        serial.writeValue("y", y)
+        serial.writeValue("fwd", fwd_speed)
+        serial.writeValue("turn", turn_speed)
+        serial.writeValue("rw", rw_speed)
+        serial.writeValue("lw", lw_speed)
     }
 })
